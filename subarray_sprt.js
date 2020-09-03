fc4ab1654d4b8f17c6c48e661a172ab9a5f9e42b
Ricky <rickdhanota@gmail.com>
2020-09-03 01:02:35 -0700

    creating the function

--- subarray_sprt.js
+++ subarray_sprt.js
@@ -1,1 +1,13 @@
 //Write a function that takes in an array of at least 2 integers and that returns an array of the starting and ending indices of the smallest subarray in the input array that needs to be sorted in place for the order for the entire input array to be sorted. for example; array = [1,2,4,7, 10,11,7,12,6,7,16,18,19, output should be [3,9]
+
+function subArray_sort(array){
+    var minOutofOrder = Infinity;
+    var maxOutofOrder = -Infinity
+    // console.log(minOutofOrder);
+    // console.log(maxOutofOrder);
+    for(var i=0; i<array.length; i++){
+
+    }
+}
+
+console.log(subArray_sort([1,2,4,7,10,11,7,12,6,7,16,18,19]));
