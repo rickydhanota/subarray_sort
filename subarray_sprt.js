e8db696ce4093d09d1d59469201ee6dd63e7126c
Ricky <rickdhanota@gmail.com>
2020-09-03 01:42:42 -0700

    created a helper function

--- subarray_sprt.js
+++ subarray_sprt.js
@@ -6,8 +6,34 @@ function subArray_sort(array){
     // console.log(minOutofOrder);
     // console.log(maxOutofOrder);
     for(var i=0; i<array.length; i++){
-
+        var num = array[i];
+        if (isOutofOrder(i, num, array)){
+            minOutofOrder = Math.min(minOutofOrder, num);
+            maxOutofOrder = Math.max(maxOutofOrder, num);
+        }
     }
+    if(minOutofOrder == Infinity){
+        return [-1, -1];
+    }
+    var subArrayLeftidx = 0;
+    while (minOutofOrder >= array[subArrayLeftidx]){
+        subArrayLeftidx++;
+    }
+    var subArrayRightidx = 0;
+    while (minOutofOrder >= array[subArrayRightidx]){
+        subArrayRightidx--;
+    }
+    return [subArrayLeftidx, subArrayRightidx];
+}
+
+function isOutofOrder(i, num, array){
+    if(i === 0){
+        return num > array[i+1];
+    }
+    if (i === array.length - 1){
+        return num < array[i-1];
+    }
+    return num > array[i+1] || num < array[i-1];
 }
 
 console.log(subArray_sort([1,2,4,7,10,11,7,12,6,7,16,18,19]));
