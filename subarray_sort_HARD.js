3601a71efa177d2d8b52770468764de768cd44e5
Ricky <rickdhanota@gmail.com>
2020-09-03 21:30:42 -0700

    changed the name

--- subarray_sort_HARD.js
+++ subarray_sort_HARD.js
@@ -1,12 +1,12 @@
 //Write a function that takes in an array of at least 2 integers and that returns an array of the starting and ending indices of the smallest subarray in the input array that needs to be sorted in place for the order for the entire input array to be sorted. for example; array = [1,2,4,7, 10,11,7,12,6,7,16,18,19, output should be [3,9]
 
 function subarraySort(array) {
-	var minOutofOrder = Infinity;
-  var maxOutofOrder = -Infinity;
-  for(var i = 0; i < array.length; i++){
-		var num = array[i];
-		if (isOutofOrder(i, num, array)){
-			minOutofOrder = Math.min(minOutofOrder, num);
+    var minOutofOrder = Infinity;
+    var maxOutofOrder = -Infinity;
+    for(var i = 0; i < array.length; i++){
+        var num = array[i];
+        if (isOutofOrder(i, num, array)){
+            minOutofOrder = Math.min(minOutofOrder, num);
 			maxOutofOrder = Math.max(maxOutofOrder, num);
 		}
 	}
@@ -25,11 +25,11 @@ function subarraySort(array) {
 }
 
 function isOutofOrder(i, num, array){
-	if(i === 0){
-		return num > array[i+1];
+    if(i === 0){
+        return num > array[i+1];
 	}
 	if (i === array.length - 1){
-		return num < array[i-1];
+        return num < array[i-1];
 	}
 	return num > array[i+1] || num < array[i-1];
 }
